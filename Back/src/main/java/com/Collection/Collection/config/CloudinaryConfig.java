44d6e07d5084c0a2b621b0f7864326f379fa09ab
VASSE Enzo <vasse.enzo27@gmail.com>
2025-12-02 12:36:07 +0100

    Remplacement des éléments par .env

--- Back/src/main/java/com/Collection/Collection/config/CloudinaryConfig.java
+++ Back/src/main/java/com/Collection/Collection/config/CloudinaryConfig.java
@@ -4,15 +4,17 @@ import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @Configuration
 public class CloudinaryConfig {
 
     @Bean
     public Cloudinary cloudinary() {
+        Dotenv dotenv = Dotenv.load();
         return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "dyuank55w",
-                "api_key", "414491594864292",
-                "api_secret", "vq3yLAXsvT_K-kpSaLBFPrzSioM"));
+                "cloud_name", dotenv.get("CLOUDINARY_CLOUD_NAME"),
+                "api_key", dotenv.get("CLOUDINARY_API_KEY"),
+                "api_secret", dotenv.get("CLOUDINARY_API_SECRET")));
     }
 }
