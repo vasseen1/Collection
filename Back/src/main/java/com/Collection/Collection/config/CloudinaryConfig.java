1fc0e8c5e710dad6d2ee322bbf26923161156efa
VASSE Enzo <vasse.enzo27@gmail.com>
2025-12-02 14:04:29 +0100

    Dockerisation avec API

--- Back/src/main/java/com/Collection/Collection/config/CloudinaryConfig.java
+++ Back/src/main/java/com/Collection/Collection/config/CloudinaryConfig.java
@@ -2,19 +2,27 @@ package com.Collection.Collection.config;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @Configuration
 public class CloudinaryConfig {
 
+    @Value("${CLOUDINARY_CLOUD_NAME}")
+    private String cloudName;
+
+    @Value("${CLOUDINARY_API_KEY}")
+    private String apiKey;
+
+    @Value("${CLOUDINARY_API_SECRET}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary() {
-        Dotenv dotenv = Dotenv.load();
         return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", dotenv.get("CLOUDINARY_CLOUD_NAME"),
-                "api_key", dotenv.get("CLOUDINARY_API_KEY"),
-                "api_secret", dotenv.get("CLOUDINARY_API_SECRET")));
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret));
     }
 }
